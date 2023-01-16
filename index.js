dba7cbbf6ea48cfab3e52337aa39c6b5992ac44a
SHREYAN DATTA GUPTA <67722966+shreyandattagupta430@users.noreply.github.com>
2023-01-16 11:30:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/*var name = "hello devs";
+console.log(name);
+
+var myMarks = 10;
+if (myMarks === 10) {
+  let myMarks = 30;
+  console.log(myMarks);
+} else {
+  console.log(myMarks);
+}
+console.log(myMarks);
+
+document.getElementById("heading").innerHTML = "Javascript DOM";
+document.querySelector(".parent").innerHTML = "Hello guys";*/
